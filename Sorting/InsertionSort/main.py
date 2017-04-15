273113fe0c001b7001afcbeaeb1ec59ad73d7af7
“[Amit <amit.kr3@hotmail.com>
2017-04-15 16:56:08 +0530

    Completed Insertion Sort

--- Sorting/InsertionSort/main.py
+++ Sorting/InsertionSort/main.py
@@ -3,13 +3,20 @@ import sys
 
 def insertionSort(list):
     """
-    input: list
+    input: Integers seperated by spaces
     output: sorted list
     created by : Amit Kumar
     """
+    for i in range(len(list)-1):
+        n = i
+        while n > -1 and list[n] > list[n+1]:
+            list[n] , list[n+1] = list[n+1] , list[n]
+            n -= 1
+    return list
 
 def main():
-    pass
+    data = [int(i) for i in sys.stdin.readline().rstrip().split(' ')]
+    sys.stdout.write(str(insertionSort(data)));
 
 
 if __name__ == '__main__':
