52f6ed908224280767f8c246fd10da6f13db2e06
“[Amit <amit.kr3@hotmail.com>
2017-04-15 16:40:28 +0530

    Insertion Sort

---
+++ Sorting/InsertionSort/main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def insertionSort(list):
+    """
+    input: list
+    output: sorted list
+    created by : Amit Kumar
+    """
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
